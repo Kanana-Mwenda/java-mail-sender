26785e15dbc136269523047d586528d0388ec5eb
Kanana-Mwenda <Lizamwenda95@gmail.com>
2025-10-21 16:53:23 +0300

    initial commit

---
+++ EmailSenderApp/SendEmail.java
@@ -0,0 +1,60 @@
+package EmailSenderApp;
+
+import java.util.*;
+import javax.mail.*;
+import javax.mail.internet.*;
+import javax.mail.Transport;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+
+public class SendEmail {
+    public static void main(String[] args) {
+
+        // Recipient and sender emails
+        String recipient = "kananamwenda20@gmail.com";
+        String sender = "lizamwenda95@gmail.com";
+
+        // Gmail SMTP host
+        String host = "smtp.gmail.com";
+
+        // Gmail authentication credentials
+        final String username = "lizamwenda95@gmail.com"; 
+        final String password = "sdzj avmm ltdn njuq";
+
+        // Set mail properties
+        Properties properties = new Properties();
+        properties.put("mail.smtp.auth", "true"); //authentication
+        properties.put("mail.smtp.starttls.enable", "true");//security protocol
+        properties.put("mail.smtp.host", host);//gmail server
+        properties.put("mail.smtp.port", "587");//port number
+
+        // Create a session with authentication
+        Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+        });
+
+        try {
+            // Mime message object
+            MimeMessage message = new MimeMessage(session);
+
+            message.setFrom(new InternetAddress(sender)); //senders email
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(recipient)); //recipients email
+            message.setSubject("Test Email using Gmail SMTP"); //subject
+            
+
+            //html template
+            String htmlTemplate = new String(Files.readAllBytes(Paths.get("EmailSenderApp/emailtemplates.html")));
+
+            message.setContent(htmlTemplate, "text/html");
+
+            // Send the message
+            Transport.send(message);
+            System.out.println(" Mail successfully sent!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
